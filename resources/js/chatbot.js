7b9bd334d57a3f96c8bb2914e8a18445ece696e5
LyonelJS <lyonel.saputra@binus.ac.id>
2024-12-03 12:11:49 +0700

    Update chatbot.js

--- resources/js/chatbot.js
+++ resources/js/chatbot.js
@@ -77,10 +77,10 @@ document.addEventListener('mousemove', (e) => {
 
     // Change the buttons to images when maxWidth is reached
     newChatButton.innerHTML = ''; // Clear existing text
-    newChatButton.innerHTML = '<img src="newChatButton.png" alt="New Chat" style="width: 50%; height: auto;">';
+    newChatButton.innerHTML = '<img src="resources/images/newChatButton.png" alt="New Chat" style="width: 50%; height: auto;">';
 
     clearHistoryButton.innerHTML = ''; // Clear existing text
-    clearHistoryButton.innerHTML = '<img src="deleteChatButton.png" alt="Clear History" style="width: 50%; height: auto;">';
+    clearHistoryButton.innerHTML = '<img src="resources/images/deleteChatButton.png" alt="Clear History" style="width: 50%; height: auto;">';
   } else { // Change back to text if less than maxWidth
     newChatButton.innerHTML = 'New Chat'; 
 
